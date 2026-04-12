a304512f5f00f475e27b5a9f9f3dc303650d52af
Kota Sato <kotasato@KotanoMacBook-Air.local>
2026-04-12 22:08:36 +0900

    fix firestore wash loading

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1775997745440,
+    __BUILD_TIMESTAMP_MS__: 1775999220812,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
